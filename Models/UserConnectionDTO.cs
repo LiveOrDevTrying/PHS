7e3a3b8108347b1e6de293c4a98f1dd3942b7487
LiveOrDevTrying <LiveOrDevTrying@gmail.com>
2019-12-04 10:46:32 -0800

    Pushed to nuget

--- Models/UserConnectionDTO.cs
+++ Models/UserConnectionDTO.cs
@@ -2,7 +2,7 @@
 
 namespace PHS.Core.Models
 {
-    public struct UserConnectionDTO : IUserConnectionDTO
+    public class UserConnectionDTO : IUserConnectionDTO
     {
         public Guid UserId { get; set; }
     }
