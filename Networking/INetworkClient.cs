edb47eee391ad3c7336bf5342141071ac87e6df9
LiveOrDevTrying <LiveOrDevTrying@gmail.com>
2019-12-22 09:06:13 -0800

    Added INetworkClient and pushed to nuget

---
+++ Networking/INetworkClient.cs
@@ -0,0 +1,21 @@
+﻿using PHS.Core.Events;
+using PHS.Core.Events.Args.NetworkEventArgs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PHS.Core.Networking
+{
+    public interface INetworkClient
+    {
+        bool IsRunning { get; }
+
+        event NetworkingEventHandler<ConnectionEventArgs> ConnectionEvent;
+        event NetworkingEventHandler<ErrorEventArgs> ErrorEvent;
+
+        bool Connect();
+        bool Disconnect();
+
+        void Dispose();
+    }
+}
