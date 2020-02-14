bf38c26936de0c00445e5dc621eedd49f9dbb94c
LiveOrDevTrying <LiveOrDevTrying@gmail.com>
2020-02-14 08:10:59 -0800

    Added server events to ICoreNetworking

--- BLL/Networking/CoreNetworking.cs
+++ BLL/Networking/CoreNetworking.cs
@@ -4,30 +4,35 @@ using System;
 
 namespace PHS.Core.BLL.Networking
 {
-    public abstract class CoreNetworking<T, U, V> : IDisposable, ICoreNetworking<T, U, V>
+    public abstract class CoreNetworking<T, U, V, W> : IDisposable, ICoreNetworking<T, U, V, W>
         where T : ConnectionEventArgs
         where U : MessageEventArgs
         where V : ErrorEventArgs
+        where W : ServerEventArgs
     {
         protected string _endOfLineCharacters;
 
         private NetworkingEventHandler<T> _connectionEvent;
         private NetworkingEventHandler<U> _messageEvent;
         private NetworkingEventHandler<V> _errorEvent;
+        private NetworkingEventHandler<W> _serverEvent;
 
-        protected virtual void FireEvent(object sender, T e)
+        protected virtual void FireEvent(object sender, T args)
         {
-            _connectionEvent?.Invoke(sender, e);
+            _connectionEvent?.Invoke(sender, args);
         }
-        protected virtual void FireEvent(object sender, U e)
+        protected virtual void FireEvent(object sender, U args)
         {
-            _messageEvent?.Invoke(sender, e);
+            _messageEvent?.Invoke(sender, args);
         }
-        protected virtual void FireEvent(object sender, V e)
+        protected virtual void FireEvent(object sender, V args)
         {
-            _errorEvent?.Invoke(sender, e);
+            _errorEvent?.Invoke(sender, args);
         }
-
+        protected virtual void FireEvent(object sender, W args)
+        {
+            _serverEvent?.Invoke(sender, args);
+        }
         public virtual void Dispose()
         { }
 
@@ -62,6 +67,17 @@ namespace PHS.Core.BLL.Networking
             remove
             {
                 _errorEvent -= value;
+            }
+        }
+        public event NetworkingEventHandler<W> ServerEvent
+        {
+            add
+            {
+                _serverEvent += value;
+            }
+            remove
+            {
+                _serverEvent -= value;
             }
         }
     }
--- BLL/Networking/ICoreNetworking.cs
+++ BLL/Networking/ICoreNetworking.cs
@@ -4,13 +4,15 @@ using System;
 
 namespace PHS.Core.BLL.Networking
 {
-    public interface ICoreNetworking<T, U, V> : IDisposable
+    public interface ICoreNetworking<T, U, V, W> : IDisposable
         where T : ConnectionEventArgs
         where U : MessageEventArgs
         where V : ErrorEventArgs
+        where W : ServerEventArgs
     {
         event NetworkingEventHandler<T> ConnectionEvent;
         event NetworkingEventHandler<U> MessageEvent;
         event NetworkingEventHandler<V> ErrorEvent;
+        event NetworkingEventHandler<W> ServerEvent;
     }
 }
