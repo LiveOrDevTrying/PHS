9e2c002a8d1ff1cd5d5be95ba3520945d8024317
LiveOrDevTrying <LiveOrDevTrying@gmail.com>
2020-06-12 07:32:23 -0700

    Updated interface on server

--- PHS.Networking.Server/Services/IUserService.cs
+++ PHS.Networking.Server/Services/IUserService.cs
@@ -5,6 +5,7 @@ namespace PHS.Networking.Server.Services
 {
     public interface IUserService<T> : IDisposable
     {
+        Task<bool> IsValidTokenAsync(string token);
         Task<T> GetIdAsync(string token);
     }
 }
