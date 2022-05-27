e9c0820bbc9b3167468ab44dbddcf9eff071fe9c
LiveOrDevTrying <liveordevtrying@gmail.com>
2022-05-27 10:45:42 -0700

    Add statics file, change generic base classes.

---
+++ PHS.Networking/Services/ICoreNetworkingGeneric.cs
@@ -0,0 +1,16 @@
+﻿using PHS.Networking.Events;
+using PHS.Networking.Events.Args;
+using System;
+
+namespace PHS.Networking.Services
+{
+    public interface ICoreNetworkingGeneric<T, U, V> : ICoreNetworking
+        where T : ConnectionEventArgs
+        where U : MessageEventArgs
+        where V : ErrorEventArgs
+    {
+        event NetworkingEventHandler<T> ConnectionEvent;
+        event NetworkingEventHandler<U> MessageEvent;
+        event NetworkingEventHandler<V> ErrorEvent;
+    }
+}
