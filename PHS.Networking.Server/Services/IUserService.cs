e500caf53b900b90c19f36ce14fcb7d5674fd922
LiveOrDevTrying <liveordevtrying@gmail.com>
2023-04-07 14:10:12 -0700

    Add function for validating token

--- PHS.Networking.Server/Services/IUserService.cs
+++ PHS.Networking.Server/Services/IUserService.cs
@@ -6,6 +6,7 @@ namespace PHS.Networking.Server.Services
 {
     public interface IUserService<UId>
     {
+        Task<bool> IsValidTokenAsync(string token, CancellationToken cancellationToken = default);
         Task<UId> GetIdAsync(string token, CancellationToken cancellationToken = default);
     }
 }
